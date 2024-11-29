4d14b759da1bcf185a771f228bed4a1b47a9f882
Subodh22 <SubodhMaharjan33@gmail.com>
2024-11-29 11:25:18 +1100

    port change

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   time            : 5, // time control (MINUTES)
   additional_time : 1,  // time advance per move (SECONDS)
 
-  port : 2000           // port on which server runs
+  port : process.env.port || 2000         // port on which server runs
 }
